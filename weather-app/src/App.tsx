962c4c4ddf5ffc02911bc264d0ca19bf2f9f45bf
Maltex <henrik.nilsson88@gmail.com>
2021-02-07 13:08:10 +0100

    project startup, added typescript, lint and prettier to project and learned about the basics.

---
+++ weather-app/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div className="App">
+            <p>Wop</p>
+        </div>
+    );
+}
+
+export default App;
